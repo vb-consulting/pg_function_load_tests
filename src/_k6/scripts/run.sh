ae9a74399dd6f7ec3134ded71bebde1ba56bfa3b
Vedran B <vbilopav@gmail.com>
2024-11-08 12:43:48 +0100

    fix name versioning

--- src/_k6/scripts/run.sh
+++ src/_k6/scripts/run.sh
@@ -8,36 +8,40 @@ echo "Results will be saved in /results/$STAMP"
 
 echo "Starting k6 tests, output will be saved in /results/$STAMP"
 
-echo "Starting django-app-v509..."
-k6 run /scripts/script.js -e STAMP=$STAMP -e TAG=django-app-v509 -e PORT=8000
+echo "Starting django-app-v5.0.9..."
+k6 run /scripts/script.js -e STAMP=$STAMP -e TAG=django-app-v5.0.9 -e PORT=8000
 
 sleep 2
-echo "Starting express-app-v4182..."
-k6 run /scripts/script.js -e STAMP=$STAMP -e TAG=express-app-v4182 -e PORT=3100
+echo "Starting express-app-v4.18.2..."
+k6 run /scripts/script.js -e STAMP=$STAMP -e TAG=express-app-v4.18.2 -e PORT=3100
 
 sleep 2
-echo "Starting postgrest-v1223..."
-k6 run /scripts/script.js -e STAMP=$STAMP -e TAG=postgrest-v1223 -e PORT=3000 -e REQ_PATH=/rpc/test_func_v1
+echo "Starting postgrest-v12.2.3..."
+k6 run /scripts/script.js -e STAMP=$STAMP -e TAG=postgrest-v12.2.3 -e PORT=3000 -e REQ_PATH=/rpc/test_func_v1
 
 sleep 2
-echo "Starting fastapi-app-v01032..."
-k6 run /scripts/script.js -e STAMP=$STAMP -e TAG=fastapi-app-v01032 -e PORT=8001
+echo "Starting fastapi-app-v0.103.2..."
+k6 run /scripts/script.js -e STAMP=$STAMP -e TAG=fastapi-app-v0.103.2 -e PORT=8001
 
 sleep 2
-echo "Starting fastify-app-v4261..."
-k6 run /scripts/script.js -e STAMP=$STAMP -e TAG=fastify-app-v4261 -e PORT=3101
+echo "Starting fastify-app-v4.26.1..."
+k6 run /scripts/script.js -e STAMP=$STAMP -e TAG=fastify-app-v4.26.1 -e PORT=3101
 
 sleep 2
-echo "Starting deno-app-v1402..."
-k6 run /scripts/script.js -e STAMP=$STAMP -e TAG=deno-app-v1402-e PORT=3102
+echo "Starting deno-app-v1.40.2..."
+k6 run /scripts/script.js -e STAMP=$STAMP -e TAG=deno-app-v1.40.2 -e PORT=3102
+
+sleep 2
+echo "Starting swoole-php-app-8.3.13..."
+k6 run /scripts/script.js -e STAMP=$STAMP -e TAG=swoole-php-app-8.3.13 -e PORT=3103
 
 sleep 2
 echo "Starting npgsqlrest-aot-v210..."
 k6 run /scripts/script.js -e STAMP=$STAMP -e TAG=npgsqlrest-aot-v210 -e PORT=5000
 
 sleep 2
-echo "Starting npgsqlrest-aot-v221..."
-k6 run /scripts/script.js -e STAMP=$STAMP -e TAG=npgsqlrest-aot-v221 -e PORT=5001
+echo "Starting npgsqlrest-aot-v2.2.1..."
+k6 run /scripts/script.js -e STAMP=$STAMP -e TAG=npgsqlrest-aot-v2.2.1 -e PORT=5001
 
 OUTPUT_FILE="/results/$STAMP.csv"
 > "$OUTPUT_FILE"
